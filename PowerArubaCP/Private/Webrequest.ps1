d3fc5ad7f932de4d52fc37e76946be95a0a448df
Alexis La Goutte <alexis.lagoutte@gmail.com>
2018-07-17 23:56:15 +0200

    WebRequest: Add Invoke-ArubaCPWebRequest for send WebRequest to ClearPass

---
+++ PowerArubaCP/Private/Webrequest.ps1
@@ -0,0 +1,63 @@
+#
+# Copyright 2018, Alexis La Goutte <alexis.lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Invoke-ArubaCPWebRequest(){
+
+    Param(
+        [Parameter(Mandatory = $true)]
+        [String]$url,
+        [Parameter(Mandatory = $true)]
+        #Valid POST, GET...
+        [String]$method,
+        [Parameter(Mandatory = $false)]
+        [psobject]$body,
+        [Parameter(Mandatory = $false)]
+        [Microsoft.PowerShell.Commands.WebRequestSession]$sessionvariable
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $Server = ${DefaultArubaCPConnection}.Server
+        $fullurl = "https://${Server}/${url}"
+
+
+        if( -Not $PsBoundParameters.ContainsKey('sessionvariable') ){
+            $sessionvariable = $DefaultArubaCPConnection.session
+            $headers = @{ Authorization = "Bearer " + $DefaultArubaCPConnection.token; Accept = "application/json," }
+        }
+        $headers
+        try {
+            if($body){
+                $response = Invoke-WebRequest $fullurl -Method $method -body ($body | ConvertTo-Json) -Headers $headers
+            } else {
+                $response = Invoke-WebRequest $fullurl -Method $method -Headers $headers
+            }
+        }
+
+        catch {
+            If ($_.Exception.Response) {
+
+                $result = $_.Exception.Response.GetResponseStream()
+                $reader = New-Object System.IO.StreamReader($result)
+                $responseBody = $reader.ReadToEnd()
+                $responseJson =  $responseBody | ConvertFrom-Json
+
+                Write-Host "The ClearPass API sends an error message:" -foreground yellow
+                Write-Host
+                Write-Host "Error description (code): $($_.Exception.Response.StatusDescription) ($($_.Exception.Response.StatusCode.Value__))" -foreground yellow
+                Write-Host "Error details: $($responseBody)" -foreground yellow
+                Write-Host
+            }
+            throw "Unable to use ClearPass API"
+        }
+        $response
+
+    }
+
+}
