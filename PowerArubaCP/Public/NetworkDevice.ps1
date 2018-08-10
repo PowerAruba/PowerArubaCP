63d47019b95d1ea0b2c43648102490bc6559fd85
Alexis La Goutte <alexis.lagoutte@gmail.com>
2018-08-10 15:55:30 +0200

    NetworkDevice: Add Add-ArubaCPNetworkDevice for adding a NAS
    name, ip_adress, radius_secret and vendor is Mandatory
    
    for vendor, there is a long (static) of autorized vendor... but no yet API to get this list...

--- PowerArubaCP/Public/NetworkDevice.ps1
+++ PowerArubaCP/Public/NetworkDevice.ps1
@@ -4,6 +4,116 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function Add-ArubaCPNetworkDevice {
+
+    <#
+        .SYNOPSIS
+        Add a Network Device (NAD) on ClearPass
+
+        .DESCRIPTION
+        Add a Network Device (NAD) with radius secret, description, coa_capable, radsec....
+
+        .EXAMPLE
+        Add-ArubaCPNetworkDevice -name SW1 -ip_address 192.0.2.1 -radius_secret MySecurePassword -vendor Aruba -description "Add by PowerArubaCP"
+
+        Add Network Device SW1 with ip address 192.0.2.1 from vendor Aruba and a description
+
+        .EXAMPLE
+        Add-ArubaCPNetworkDevice -name SW2 -ip_address 192.0.2.2 -radius_secret MySecurePassword -vendor Aruba -coa_capable -coa_port 5000
+
+        Add Network Device SW2 with COA Capability on port 5000
+
+        .EXAMPLE
+        Add-ArubaCPNetworkDevice -name SW3 -ip_address 192.0.2.3 -radius_secret MySecurePassword -vendor Cisco -tacacs_secret MySecurePassword
+
+        Add Network Device SW3 with a tacacs secret from vendor Cisco
+
+        .EXAMPLE
+        Add-ArubaCPNetworkDevice -name SW4 -ip_address 192.0.2.4 -radius_secret MySecurePassword -vendor Hewlett-Packard-Enterprise -radsec_enabled
+
+        Add Network Device SW4 with RadSec from vendor HPE
+    #>
+
+    Param(
+        [Parameter (Mandatory=$false)]
+        [int]$id,
+        [Parameter (Mandatory=$false)]
+        [string]$description,
+        [Parameter (Mandatory=$true)]
+        [string]$name,
+        [Parameter (Mandatory=$true)]
+        [ipaddress]$ip_address,
+        [Parameter (Mandatory=$true)]
+        [string]$radius_secret,
+        [Parameter (Mandatory=$false)]
+        [string]$tacacs_secret,
+        [Parameter (Mandatory=$true)]
+        [string]$vendor_name,
+        [Parameter (Mandatory=$false)]
+        [switch]$coa_capable,
+        [Parameter (Mandatory=$false)]
+        [int]$coa_port,
+        [Parameter (Mandatory=$false)]
+        [switch]$radsec_enabled
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $url = "api/network-device"
+
+        $_nad = new-Object -TypeName PSObject
+
+        if ( $PsBoundParameters.ContainsKey('id') ) {
+            $_nad | add-member -name "id" -membertype NoteProperty -Value $id
+        }
+
+        if ( $PsBoundParameters.ContainsKey('description') ) {
+            $_nad | add-member -name "description" -membertype NoteProperty -Value $description
+        }
+
+        $_nad | add-member -name "name" -membertype NoteProperty -Value $name
+
+        $_nad | add-member -name "ip_address" -membertype NoteProperty -Value $ip_address.ToString()
+
+        $_nad | add-member -name "radius_secret" -membertype NoteProperty -Value $radius_secret
+
+        if ( $PsBoundParameters.ContainsKey('tacacs_secret') ) {
+            $_nad | add-member -name "tacacs_secret" -membertype NoteProperty -Value $tacacs_secret
+        }
+
+        $_nad | add-member -name "vendor_name" -membertype NoteProperty -Value $vendor_name
+
+        if ( $PsBoundParameters.ContainsKey('coa_capable') ) {
+            if ( $coa_capable ) {
+                $_nad | add-member -name "coa_capable" -membertype NoteProperty -Value $True
+            } else {
+                $_nad | add-member -name "coa_capable" -membertype NoteProperty -Value $false
+            }
+        }
+
+        if ( $PsBoundParameters.ContainsKey('coa_port') ) {
+            $_nad | add-member -name "coa_port" -membertype NoteProperty -Value $coa_port
+        }
+
+        if ( $PsBoundParameters.ContainsKey('radsec_enabled') ) {
+            if ( $radsec_enabled ) {
+                $_nad | add-member -name "radsec_enabled" -membertype NoteProperty -Value $True
+            } else {
+                $_nad | add-member -name "radsec_enabled" -membertype NoteProperty -Value $false
+            }
+        }
+
+        $nad = invoke-ArubaCPRestMethod -method "POST" -body $_nad -uri $url
+        $nad
+    }
+
+    End {
+    }
+}
+
 function Get-ArubaCPNetworkDevice {
 
     <#
