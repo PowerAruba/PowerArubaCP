1ced97baa7b832904e26d28b53f7afc8b10b14c3
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-08-19 13:55:51 +0200

    Tests: Add credential(.example).ps1

---
+++ Tests/credential.example.ps1
@@ -0,0 +1,10 @@
+#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Copy this file to credential.ps1 (on Tests folder) and change connection settings..
+
+$ipaddress = "10.44.23.213"
+$token = "aaaaaaaaaaaaaaaaaa"
