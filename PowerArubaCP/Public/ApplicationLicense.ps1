d4229bd5dc1bab2b190cb4cb0c172378de379829
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-01-09 16:27:24 +0100

    ApplicationLicense: Fix filtering for licensing
    Use PowerShell filtering with id, product_name (Access, Entry..) and Licence Type (Evaluation or Permament)

--- PowerArubaCP/Public/ApplicationLicense.ps1
+++ PowerArubaCP/Public/ApplicationLicense.ps1
@@ -19,13 +19,33 @@ function Get-ArubaCPApplicationLicense {
         Get ALL Application License  on the Clearpass
 
         .EXAMPLE
+        Get-ArubaCPApplicationLicense -id 3001
+
+        Get info about Application License where id equal 3001
+
+        .EXAMPLE
         Get-ArubaCPApplicationLicense -product_name Access
 
         Get info about Application License where product_name is Access
 
+        .EXAMPLE
+        Get-ArubaCPApplicationLicense -license_type Evaluation
+
+        Get info about Application License where license type is Evaluation
+
     #>
 
+    [CmdLetBinding(DefaultParameterSetName = "Default")]
+
     Param(
+        [Parameter (Mandatory = $false, ParameterSetName = "id")]
+        [int]$id,
+        [Parameter (Mandatory = $false, ParameterSetName = "product_name")]
+        [ValidateSet('Access', 'Access Upgrade', 'Entry', 'Onboard', 'OnGuard', IgnoreCase = $false)]
+        [string]$product_name,
+        [Parameter (Mandatory = $false, ParameterSetName = "license_type")]
+        [ValidateSet('Evaluation', 'Permanent', IgnoreCase = $false)]
+        [string]$license_type
     )
 
     Begin {
@@ -35,9 +55,14 @@ function Get-ArubaCPApplicationLicense {
 
         $url = "api/application-license"
 
-        $al = Invoke-ArubaCPRestMethod -method "GET" -uri $url @invokeParams
+        $al = Invoke-ArubaCPRestMethod -method "GET" -uri $url
 
-        $al._embedded.items
+        switch ( $PSCmdlet.ParameterSetName ) {
+            "id" { $al._embedded.items | Where-Object { $_.id -eq $id } }
+            "product_name" { $al._embedded.items | Where-Object { $_.product_name -eq $product_name } }
+            "license_type" { $al._embedded.items | Where-Object { $_.license_type -eq $license_type } }
+            default { $al._embedded.items }
+        }
     }
 
     End {
