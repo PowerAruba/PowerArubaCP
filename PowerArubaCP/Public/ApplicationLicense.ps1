d30fc1adfe17a685aaf056cf74a8f45ed818aaee
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-01-10 14:06:53 +0100

    ApplicationLicense: Add Remove-ArubaCPApplicationLicense for remove Application License

--- PowerArubaCP/Public/ApplicationLicense.ps1
+++ PowerArubaCP/Public/ApplicationLicense.ps1
@@ -114,3 +114,67 @@ function Get-ArubaCPApplicationLicense {
     End {
     }
 }
+
+function Remove-ArubaCPApplicationLicense {
+
+    <#
+        .SYNOPSIS
+        Remove an Application License on ClearPass
+
+        .DESCRIPTION
+        Remove an Application License) on ClearPass
+
+        .EXAMPLE
+        $al = Get-ArubaCPApplicationLicense -product_name Access
+        PS C:\>$al | Remove-ArubaCPApplicationLicense
+
+        Remove Application License type Access
+
+        .EXAMPLE
+        Remove-ArubaCPApplicationLicense -id 3001 -noconfirm
+
+        Remove Application License id 3001 with no confirmation
+    #>
+
+    Param(
+        [Parameter (Mandatory = $true, ParameterSetName = "id")]
+        [int]$id,
+        [Parameter (Mandatory = $true, ValueFromPipeline = $true, Position = 1, ParameterSetName = "al")]
+        [ValidateScript( { Confirm-ArubaCPApplicationLicense $_ })]
+        [psobject]$al,
+        [Parameter(Mandatory = $false)]
+        [switch]$noconfirm
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        #get nad id from nad ps object
+        if ($al) {
+            $id = $al.id
+        }
+
+        $url = "api/application-license/${id}"
+
+        if ( -not ( $Noconfirm )) {
+            $message = "Remove Application License on ClearPass"
+            $question = "Proceed with removal of Application License ${id} ?"
+            $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+            $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+            $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+            $decision = $Host.UI.PromptForChoice($message, $question, $choices, 1)
+        }
+        else { $decision = 0 }
+        if ($decision -eq 0) {
+            Write-Progress -activity "Remove Application License"
+            Invoke-ArubaCPRestMethod -method "DELETE" -uri $url
+            Write-Progress -activity "Remove Application License" -completed
+        }
+    }
+
+    End {
+    }
+}
