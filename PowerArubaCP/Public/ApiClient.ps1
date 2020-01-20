b123482d14ed6dc8c14b8b9ee42d052e0218fdc8
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-01-20 12:56:00 +0100

    ApiClient: Add Remove-ArubaCPClient

--- PowerArubaCP/Public/ApiClient.ps1
+++ PowerArubaCP/Public/ApiClient.ps1
@@ -215,3 +215,70 @@ function Get-ArubaCPApiClient {
     End {
     }
 }
+
+function Remove-ArubaCPApiClient {
+
+    <#
+        .SYNOPSIS
+        Remove an ApiClient on ClearPass
+
+        .DESCRIPTION
+        Remove an ApiClient on ClearPass
+
+        .EXAMPLE
+        $ac = Get-ArubaCPApiClient -client_id PowerArubaCP
+        PS C:\>$ac | Remove-ArubaCPApiClient
+
+        Remove API Client with client id PowerArubaCP
+
+        .EXAMPLE
+        Remove-ArubaCPEndpoint -id 3001 -noconfirm
+
+        Remove API Client with id 3001 and no confirmation
+    #>
+
+    Param(
+        [Parameter (Mandatory = $true, ParameterSetName = "id")]
+        [string]$id,
+        [Parameter (Mandatory = $true, ValueFromPipeline = $true, Position = 1, ParameterSetName = "ac")]
+        [ValidateScript( { Confirm-ArubaCPApiClient $_ })]
+        [psobject]$ac,
+        [Parameter(Mandatory = $false)]
+        [switch]$noconfirm,
+        [Parameter (Mandatory = $False)]
+        [ValidateNotNullOrEmpty()]
+        [PSObject]$connection = $DefaultArubaCPConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        #get ApiClient id from nad ps object
+        if ($ac) {
+            $id = $ac.client_id
+        }
+
+        $uri = "api/api-client/${id}"
+
+        if ( -not ( $Noconfirm )) {
+            $message = "Remove API Client on ClearPass"
+            $question = "Proceed with removal of API Client ${id} ?"
+            $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+            $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+            $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+            $decision = $Host.UI.PromptForChoice($message, $question, $choices, 1)
+        }
+        else { $decision = 0 }
+        if ($decision -eq 0) {
+            Write-Progress -activity "Remove API Client"
+            Invoke-ArubaCPRestMethod -method "DELETE" -uri $uri -connection $connection
+            Write-Progress -activity "Remove API Client" -completed
+        }
+    }
+
+    End {
+    }
+}
