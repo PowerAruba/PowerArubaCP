e7aa4a159395c7b11ffd3d9b131e61d3e4815834
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-01-23 06:46:45 +0100

    Services(Tests): skip tests when use ClearPass <= 6.8.0

--- Tests/integration/Services.Tests.ps1
+++ Tests/integration/Services.Tests.ps1
@@ -7,18 +7,18 @@
 
 Describe  "Get Service" {
 
-    It "Get Service Does not throw an error" {
+    It "Get Service Does not throw an error" -Skip:$VersionBefore680 {
         {
             Get-ArubaCPService
         } | Should Not Throw
     }
 
-    It "Get Service " {
+    It "Get Service " -Skip:$VersionBefore680 {
         $s = Get-ArubaCPService
         $s.count | Should not be $NULL
     }
 
-    It "Get Service id (id 1)" {
+    It "Get Service id (id 1)" -Skip:$VersionBefore680 {
         $s = Get-ArubaCPService | Where-Object { $_.id -eq "1" }
         $s.id | Should be "1"
         $s.name | Should be "[Policy Manager Admin Network Login Service]"
@@ -28,26 +28,26 @@ Describe  "Get Service" {
         $s.orderNo | Should be "1"
     }
 
-    It "Get Network Device (id 1) and confirm (via Confirm-ArubaCPService)" {
+    It "Get Network Device (id 1) and confirm (via Confirm-ArubaCPService)" -Skip:$VersionBefore680 {
         $s = Get-ArubaCPService | Where-Object { $_.id -eq "1" }
         Confirm-ArubaCPService $s | Should be $true
     }
 
-    It "Search Service by id (1)" {
+    It "Search Service by id (1)" -Skip:$VersionBefore680 {
         $s = Get-ArubaCPService -id 1
         @($s).count | Should be 1
         $s.id | Should not be BeNullOrEmpty
         $s.name | Should be "[Policy Manager Admin Network Login Service]"
     }
 
-    It "Search Service by id ([Policy Manager Admin Network Login Service])" {
+    It "Search Service by id ([Policy Manager Admin Network Login Service])" -Skip:$VersionBefore680 {
         $s = Get-ArubaCPService -name '[Policy Manager Admin Network Login Service]'
         @($s).count | Should be 1
         $s.id | Should not be BeNullOrEmpty
         $s.name | Should be "[Policy Manager Admin Network Login Service]"
     }
 
-    It "Search Network Device by name (contains *Policy*)" {
+    It "Search Network Device by name (contains *Policy*)" -Skip:$VersionBefore680 {
         $s = Get-ArubaCPService -name Policy -filter_type contains
         @($s).count | Should be 1
         $s.id | Should not be BeNullOrEmpty
@@ -65,14 +65,14 @@ Describe  "Get Service" {
 
 Describe  "Enable / Disable Service" {
 
-    It "Disable Service (id 1)" {
+    It "Disable Service (id 1)" -Skip:$VersionBefore680 {
         $s = Get-ArubaCPService -id 1
         $s.enabled | Should be "True"
         $s = Get-ArubaCPService -id 1 | Disable-ArubaCPService
         $s.enabled | Should be "false"
     }
 
-    It "Enable Service (id 1)" {
+    It "Enable Service (id 1)" -Skip:$VersionBefore680 {
         $s = Get-ArubaCPService -id 1 | Enable-ArubaCPService
         $s.enabled | Should be "true"
     }
