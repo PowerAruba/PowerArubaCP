051484c41c303e56ed2eaef0f7ed357b1651a3d3
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-02-20 20:53:35 +0100

    NetworkDevices(Tests): add Compability with Pester v5
        be => -Be
        not => -Not
        Throw => -Throw
        BeNullOrEmpty => -BeNullOrEmpty

--- Tests/integration/NetworkDevice.Tests.ps1
+++ Tests/integration/NetworkDevice.Tests.ps1
@@ -16,47 +16,47 @@ Describe  "Get Network Device" {
     It "Get NetworkDevice Does not throw an error" {
         {
             Get-ArubaCPNetworkDevice
-        } | Should Not Throw
+        } | Should -Not -Throw
     }
 
     It "Get ALL Network Device" {
         $nad = Get-ArubaCPNetworkDevice
-        $nad.count | Should not be $NULL
+        $nad.count | Should -Not -Be $NULL
     }
 
     It "Get Network Device (pester_SW1)" {
         $nad = Get-ArubaCPNetworkDevice | Where-Object { $_.name -eq "pester_SW1" }
-        $nad.id | Should not be BeNullOrEmpty
-        $nad.name | Should be "pester_SW1"
-        $nad.description | Should be "Add by PowerArubaCP"
-        $nad.ip_address | Should be "192.0.2.1"
-        $nad.vendor_name | Should be "Aruba"
-        $nad.coa_capable | Should be "False"
-        $nad.coa_port | Should be "3799"
+        $nad.id | Should -Not -Be BeNullOrEmpty
+        $nad.name | Should -Be "pester_SW1"
+        $nad.description | Should -Be "Add by PowerArubaCP"
+        $nad.ip_address | Should -Be "192.0.2.1"
+        $nad.vendor_name | Should -Be "Aruba"
+        $nad.coa_capable | Should -Be "False"
+        $nad.coa_port | Should -Be "3799"
     }
 
     It "Get Network Device (pester_SW1) and confirm (via Confirm-ArubaCPNetworkDevice)" {
         $nad = Get-ArubaCPNetworkDevice | Where-Object { $_.name -eq "pester_SW1" }
-        Confirm-ArubaCPNetworkDevice $nad | Should be $true
+        Confirm-ArubaCPNetworkDevice $nad | Should -Be $true
     }
 
     It "Search Network Device by name (pester_SW2)" {
         $nad = Get-ArubaCPNetworkDevice -name pester_SW2
-        @($nad).count | Should be 1
-        $nad.id | Should not be BeNullOrEmpty
-        $nad.name | Should be "pester_SW2"
-        $nad.description | Should be "Add by PowerArubaCP"
+        @($nad).count | Should -Be 1
+        $nad.id | Should -Not -Be BeNullOrEmpty
+        $nad.name | Should -Be "pester_SW2"
+        $nad.description | Should -Be "Add by PowerArubaCP"
     }
 
     It "Search Network Device by name (contains *pester*)" {
         $nad = Get-ArubaCPNetworkDevice -name pester -filter_type contains
-        @($nad).count | Should be 2
+        @($nad).count | Should -Be 2
     }
 
     It "Search Network Device by attribute (ip_address equal 192.0.2.1)" {
         $nad = Get-ArubaCPNetworkDevice -filter_attribute ip_address -filter_type equal -filter_value 192.0.2.1
-        @($nad).count | Should be 1
-        $nad.ip_address | Should be "192.0.2.1"
+        @($nad).count | Should -Be 1
+        $nad.ip_address | Should -Be "192.0.2.1"
     }
 
     AfterAll {
@@ -71,21 +71,21 @@ Describe  "Add Network Device" {
     It "Add Network Device with Coa Enable and Change Port" {
         Add-ArubaCPNetworkDevice -name pester_SW1 -ip_address 192.0.2.1 -radius_secret MySecurePassword -vendor Aruba -coa_capable -coa_port 5000
         $nad = Get-ArubaCPNetworkDevice -name pester_SW1
-        $nad.id | Should not be BeNullOrEmpty
-        $nad.name | Should be "pester_SW1"
-        $nad.ip_address | Should be "192.0.2.1"
-        $nad.vendor_name | Should be "Aruba"
-        $nad.coa_capable | Should be "true"
-        $nad.coa_port | Should be "5000"
+        $nad.id | Should -Not -Be BeNullOrEmpty
+        $nad.name | Should -Be "pester_SW1"
+        $nad.ip_address | Should -Be "192.0.2.1"
+        $nad.vendor_name | Should -Be "Aruba"
+        $nad.coa_capable | Should -Be "true"
+        $nad.coa_port | Should -Be "5000"
     }
 
     It "Add Network Device with TACACS secret (and Cisco vendor)" {
         Add-ArubaCPNetworkDevice -name pester_SW1 -ip_address 192.0.2.1 -radius_secret MySecurePassword -vendor Cisco -tacacs_secret MySecurePassword
         $nad = Get-ArubaCPNetworkDevice -name pester_SW1
-        $nad.id | Should not be BeNullOrEmpty
-        $nad.name | Should be "pester_SW1"
-        $nad.ip_address | Should be "192.0.2.1"
-        $nad.vendor_name | Should be "Cisco"
+        $nad.id | Should -Not -Be BeNullOrEmpty
+        $nad.name | Should -Be "pester_SW1"
+        $nad.ip_address | Should -Be "192.0.2.1"
+        $nad.vendor_name | Should -Be "Cisco"
         # radius_secret and tacacs_secret are always empty...
     }
 
@@ -93,11 +93,11 @@ Describe  "Add Network Device" {
     #It "Add Network Device with RadSec" {
     #    Add-ArubaCPNetworkDevice -name pester_SW1 -ip_address 192.0.2.1 -radius_secret MySecurePassword -vendor Aruba -radsec_enabled
     #    $nad = Get-ArubaCPNetworkDevice -name pester_SW1
-    #    $nad.id | Should not be BeNullOrEmpty
-    #    $nad.name | Should be "pester_SW1"
-    #    $nad.ip_address | Should be "192.0.2.1"
-    #    $nad.vendor_name | Should be "Aruba"
-    #    $nad.radsec_enabled | Should be true
+    #    $nad.id | Should -Not -Be BeNullOrEmpty
+    #    $nad.name | Should -Be "pester_SW1"
+    #    $nad.ip_address | Should -Be "192.0.2.1"
+    #    $nad.vendor_name | Should -Be "Aruba"
+    #    $nad.radsec_enabled | Should -Be true
     #}
 
     AfterEach {
@@ -114,35 +114,35 @@ Describe  "Configure Network Device" {
     It "Rename Network Device (pester_SW1 => pester_SW2) and description" {
         Get-ArubaCPNetworkDevice -name pester_SW1 | Set-ArubaCPNetworkDevice -name pester_SW2 -description "Modified by PowerArubaCP"
         $nad = Get-ArubaCPNetworkDevice -name pester_SW2
-        $nad.id | Should not be BeNullOrEmpty
-        $nad.name | Should be "pester_SW2"
-        $nad.description | Should be "Modified by PowerArubaCP"
-        $nad.ip_address | Should be "192.0.2.1"
-        $nad.vendor_name | Should be "Aruba"
-        $nad.coa_capable | Should be "false"
-        $nad.coa_port | Should be "3799"
+        $nad.id | Should -Not -Be BeNullOrEmpty
+        $nad.name | Should -Be "pester_SW2"
+        $nad.description | Should -Be "Modified by PowerArubaCP"
+        $nad.ip_address | Should -Be "192.0.2.1"
+        $nad.vendor_name | Should -Be "Aruba"
+        $nad.coa_capable | Should -Be "false"
+        $nad.coa_port | Should -Be "3799"
     }
 
     It "Reconfigure Network Device (Change ip_address => 192.0.2.2 and Vendor => HPE)" {
         Get-ArubaCPNetworkDevice -name pester_SW2 | Set-ArubaCPNetworkDevice -ip_address 192.0.2.2 -vendor "Hewlett-Packard-Enterprise"
         $nad = Get-ArubaCPNetworkDevice -name pester_SW2
-        $nad.id | Should not be BeNullOrEmpty
-        $nad.ip_address | Should be "192.0.2.2"
-        $nad.vendor_name | Should be "Hewlett-Packard-Enterprise"
+        $nad.id | Should -Not -Be BeNullOrEmpty
+        $nad.ip_address | Should -Be "192.0.2.2"
+        $nad.vendor_name | Should -Be "Hewlett-Packard-Enterprise"
     }
 
     It "Reconfigure Network Device (Enable COA and Change COA Port to 6000)" {
         Get-ArubaCPNetworkDevice -name pester_SW2 | Set-ArubaCPNetworkDevice -coa_capable -coa_port 6000
         $nad = Get-ArubaCPNetworkDevice -name pester_SW2
-        $nad.id | Should not be BeNullOrEmpty
-        $nad.coa_capable | Should be "true"
-        $nad.coa_port | Should be "6000"
+        $nad.id | Should -Not -Be BeNullOrEmpty
+        $nad.coa_capable | Should -Be "true"
+        $nad.coa_port | Should -Be "6000"
     }
 
     It "Reconfigure Network Device (Change RADIUS and TACACS secret)" {
         Get-ArubaCPNetworkDevice -name pester_SW2 | Set-ArubaCPNetworkDevice -radius_secret MySecurePassword -tacacs_secret MySecurePassword
         $nad = Get-ArubaCPNetworkDevice -name pester_SW2
-        $nad.id | Should not be BeNullOrEmpty
+        $nad.id | Should -Not -Be BeNullOrEmpty
         # radius_secret and tacacs_secret are always empty...
     }
 
@@ -150,8 +150,8 @@ Describe  "Configure Network Device" {
     #It "Reconfigre Network Device (Enable RadSec)" {
     #    Get-ArubaCPNetworkDevice -name pester_SW2 | Set-ArubaCPNetworkDevice -radsec_enabled
     #    $nad = Get-ArubaCPNetworkDevice -name pester_SW2
-    #    $nad.id | Should not be BeNullOrEmpty
-    #    $nad.radsec_enabled | Should be true
+    #    $nad.id | Should -Not -Be BeNullOrEmpty
+    #    $nad.radsec_enabled | Should -Be true
     #}
 
     AfterAll {
@@ -164,23 +164,23 @@ Describe  "Remove Network Device" {
     It "Remove Network Device by id" {
         Add-ArubaCPNetworkDevice -name pester_SW1 -ip_address 192.0.2.1 -radius_secret MySecurePassword -vendor Aruba
         $nad = Get-ArubaCPNetworkDevice -name pester_SW1
-        $nad.name | Should be "pester_SW1"
-        @($nad).count | should be 1
+        $nad.name | Should -Be "pester_SW1"
+        @($nad).count | Should -Be 1
         Remove-ArubaCPNetworkDevice -id $nad.id -noconfirm
         $nad = Get-ArubaCPNetworkDevice -name pester_SW1
-        $nad | Should BeNullOrEmpty
-        @($nad).count | should be 0
+        $nad | Should -BeNullOrEmpty
+        @($nad).count | Should -Be 0
     }
 
     It "Remove Network Device by name (and pipeline)" {
         Add-ArubaCPNetworkDevice -name pester_SW1 -ip_address 192.0.2.1 -radius_secret MySecurePassword -vendor Aruba
         $nad = Get-ArubaCPNetworkDevice -name pester_SW1
-        $nad.name | Should be "pester_SW1"
-        @($nad).count | should be 1
+        $nad.name | Should -Be "pester_SW1"
+        @($nad).count | Should -Be 1
         Get-ArubaCPNetworkDevice -name pester_SW1 | Remove-ArubaCPNetworkDevice -noconfirm
         $nad = Get-ArubaCPNetworkDevice -name pester_SW1
-        $nad | Should BeNullOrEmpty
-        @($nad).count | should be 0
+        $nad | Should -BeNullOrEmpty
+        @($nad).count | Should -Be 0
     }
 
     AfterEach {
