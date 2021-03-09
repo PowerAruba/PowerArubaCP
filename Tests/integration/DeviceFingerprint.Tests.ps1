fc2178819f2d5a6c8e5a73c6367d79850a290c30
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-03-09 22:38:17 +0100

    DeviceFingerprint(Tests): Add Test for Add (by hostname, ip_address, device)
    Need to use different mac address and delay (2 secs) for profiling...

--- Tests/integration/DeviceFingerprint.Tests.ps1
+++ Tests/integration/DeviceFingerprint.Tests.ps1
@@ -62,6 +62,53 @@ Describe  "Get Device FingerPrint" {
     }
 }
 
+Describe  "Add Device Fingerprint" {
+
+    It "Add Device Fingerprint with hostname" {
+        Add-ArubaCPDeviceFingerprint -mac_address 00-01-02-03-04-07 -hostname pester_PowerArubaCP
+        Start-Sleep 2
+        $dfp = Get-ArubaCPDeviceFingerprint -mac_address 00-01-02-03-04-07
+        $dfp.hostname | Should -be "pester_PowerArubaCP"
+        $dfp.updated_at | Should -Not -BeNullOrEmpty
+        $dfp.added_at | Should -Not -BeNullOrEmpty
+        $dfp.mac | Should -Be "000102030407"
+        $dfp.device_category | Should -Not -BeNullOrEmpty
+        $dfp.device_name | Should -Not -BeNullOrEmpty
+        $dfp.device_family | Should -Not -BeNullOrEmpty
+    }
+
+    It "Add Device Fingerprint with IP (Address)" {
+        Add-ArubaCPDeviceFingerprint -mac_address 00-01-02-03-04-08 -ip_address 192.0.2.1
+        Start-Sleep 2
+        $dfp = Get-ArubaCPDeviceFingerprint -mac_address 00-01-02-03-04-08
+        $dfp.ip | Should -be "192.0.2.1"
+        $dfp.updated_at | Should -Not -BeNullOrEmpty
+        $dfp.added_at | Should -Not -BeNullOrEmpty
+        $dfp.mac | Should -Be "000102030408"
+        $dfp.device_category | Should -Not -BeNullOrEmpty
+        $dfp.device_name | Should -Not -BeNullOrEmpty
+        $dfp.device_family | Should -Not -BeNullOrEmpty
+    }
+
+    It "Add Device Fingerprint with device information" {
+        Add-ArubaCPDeviceFingerprint -mac_address 00-01-02-03-04-09 -device_category Server -device_family ClearPass -device_name ClearPass VM
+        Start-Sleep 2
+        $dfp = Get-ArubaCPDeviceFingerprint -mac_address 00-01-02-03-04-09
+        $dfp.updated_at | Should -Not -BeNullOrEmpty
+        $dfp.added_at | Should -Not -BeNullOrEmpty
+        $dfp.mac | Should -Be "000102030409"
+        $dfp.device_category | Should -Be "Server"
+        $dfp.device_name | Should -Be "Clearpass"
+        $dfp.device_family | Should -Be "ClearPass"
+    }
+
+    AfterAll {
+        Get-ArubaCPEndpoint -mac_address 00-01-02-03-04-07 | Remove-ArubaCPEndpoint -confirm:$false
+        Get-ArubaCPEndpoint -mac_address 00-01-02-03-04-08 | Remove-ArubaCPEndpoint -confirm:$false
+        Get-ArubaCPEndpoint -mac_address 00-01-02-03-04-09 | Remove-ArubaCPEndpoint -confirm:$false
+    }
+}
+
 AfterAll {
     Disconnect-ArubaCP -confirm:$false
 }
