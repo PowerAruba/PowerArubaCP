87c2799a2c56455a14b7f9d39f22a61c6a03e9b9
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-04-11 16:13:23 +0200

    AuthMethod: Add cmdlet for Get Auth(entication) Method
    PAP, EAP....

---
+++ PowerArubaCP/Public/AuthMethod.ps1
@@ -0,0 +1,119 @@
+#
+# Copyright 2021, Alexis La Goutte <alexis.lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Get-ArubaCPAuthMethod {
+
+    <#
+        .SYNOPSIS
+        Get Authentication Method on CPPM
+
+        .DESCRIPTION
+        Get Authentication Method (Id, Name, Description, Method, Details ...)
+
+        .EXAMPLE
+        Get-ArubaCPAuthMethod
+
+        Get ALL Authentication Method on the Clearpass
+
+        .EXAMPLE
+        Get-ArubaCPAuthMethod [PAP]
+
+        Get info about Auth Method [PAP] on the ClearPass
+
+        .EXAMPLE
+        Get-ArubaCPAuthMethod -id 23
+
+        Get info about Auth Method id 23 on the ClearPass
+
+        .EXAMPLE
+        Get-ArubaCPAuthMethod PAP -filter_type contains
+
+        Get info about Auth Method where name contains PAP
+
+       .EXAMPLE
+        Get-ArubaCPAuthMethod -filter_attribute method_type -filter_type equal -filter_value PAP
+
+        Get info about Auth Method where method_type equal pap
+
+    #>
+
+    [CmdLetBinding(DefaultParameterSetName = "Default")]
+
+    Param(
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "id")]
+        [int]$id,
+        [Parameter (Mandatory = $false, Position = 1)]
+        [Parameter (ParameterSetName = "name")]
+        [string]$Name,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [string]$filter_attribute,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "id")]
+        [Parameter (ParameterSetName = "name")]
+        [Parameter (ParameterSetName = "filter")]
+        [ValidateSet('equal', 'contains')]
+        [string]$filter_type,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [psobject]$filter_value,
+        [Parameter (Mandatory = $false)]
+        [int]$limit,
+        [Parameter (Mandatory = $False)]
+        [ValidateNotNullOrEmpty()]
+        [PSObject]$connection = $DefaultArubaCPConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('limit') ) {
+            $invokeParams.add( 'limit', $limit )
+        }
+
+        switch ( $PSCmdlet.ParameterSetName ) {
+            "id" {
+                $filter_value = $id
+                $filter_attribute = "id"
+            }
+            "name" {
+                $filter_value = $name
+                $filter_attribute = "name"
+            }
+            default { }
+        }
+
+        if ( $PsBoundParameters.ContainsKey('filter_type') ) {
+            switch ( $filter_type ) {
+                "equal" {
+                    $filter_value = @{ "`$eq" = $filter_value }
+                }
+                "contains" {
+                    $filter_value = @{ "`$contains" = $filter_value }
+                }
+                default { }
+            }
+        }
+
+        if ($filter_value -and $filter_attribute) {
+            $filter = @{ $filter_attribute = $filter_value }
+            $invokeParams.add( 'filter', $filter )
+        }
+
+        $uri = "api/auth-method"
+
+        $as = Invoke-ArubaCPRestMethod -method "GET" -uri $uri @invokeParams -connection $connection
+
+        $as._embedded.items
+    }
+
+    End {
+    }
+}
