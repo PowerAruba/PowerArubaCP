a34c51869b6b2fbc5689b3c20be9697bb9fee713
Alexis La Goutte <alexis.lagoutte@gmail.com>
2023-12-08 17:27:54 +0100

    Connection(Tests): Add Multiple connection cluster certificat test

--- Tests/integration/Connection.Tests.ps1
+++ Tests/integration/Connection.Tests.ps1
@@ -92,6 +92,9 @@ Describe "Connect to a ClearPass (using multi connection)" {
         It "Use Multi connection for call Get Local User" {
             { Get-ArubaCPLocalUser -connection $cppm } | Should -Not -Throw
         }
+        It "Use Multi connection for call Get Cluster Certificate" {
+            { Get-ArubaCPClusterCertificate -connection $cppm } | Should -Not -Throw
+        }
     }
 
     It "Disconnect to a ClearPass (Multi connection)" {
