eebcf9bca4f2982e99deba52c704bc06b01b2ed4
Alexis La Goutte <alexis.lagoutte@gmail.com>
2023-12-08 17:40:25 +0100

    Certificate(Tests): Use -skip for skip some tests

--- Tests/common.ps1
+++ Tests/common.ps1
@@ -35,6 +35,7 @@ $script:MyNewSecurePassword = ConvertTo-SecureString MyNewassword -AsPlainText -
 $script:VersionBefore680 = $DefaultArubaCPConnection.Version -lt [version]"6.8.0"
 $script:VersionBefore686 = $DefaultArubaCPConnection.Version -lt [version]"6.8.6"
 $script:VersionBefore690 = $DefaultArubaCPConnection.Version -lt [version]"6.9.0"
+$script:VersionBefore6100 = $DefaultArubaCPConnection.Version -lt [version]"6.10.0"
 
 $script:server_uuid = (Get-ArubaCPServerConfiguration).server_uuid[0]
 
