c9824739a30967b5ba4aedeff17717dfe80f2cfc
Alexis La Goutte <alexis.lagoutte@gmail.com>
2024-01-29 20:50:31 +0100

    Certificate(Tests/common): fix to get the first entry

--- Tests/common.ps1
+++ Tests/common.ps1
@@ -37,6 +37,6 @@ $script:VersionBefore686 = $DefaultArubaCPConnection.Version -lt [version]"6.8.6
 $script:VersionBefore690 = $DefaultArubaCPConnection.Version -lt [version]"6.9.0"
 $script:VersionBefore6100 = $DefaultArubaCPConnection.Version -lt [version]"6.10.0"
 
-$script:server_uuid = (Get-ArubaCPServerConfiguration).server_uuid[0]
+$script:server_uuid = (Get-ArubaCPServerConfiguration)[0].server_uuid
 
 Disconnect-ArubaCP -confirm:$false
