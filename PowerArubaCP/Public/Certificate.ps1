78583577ac8c2d81f21ecdb27cef3478816ede67
Alexis La Goutte <alexis.lagoutte@gmail.com>
2026-09-16 16:41:52 +0200

    Certificate(Add Server): Fix .Example

--- PowerArubaCP/Public/Certificate.ps1
+++ PowerArubaCP/Public/Certificate.ps1
@@ -128,9 +128,9 @@ function Add-ArubaCPServerCertificate {
         .EXAMPLE
         $passphrase = ConvertTo-SecureString mypassword -AsPlainText -Force
         PS > $server_uuid = (Get-ArubaCPServerConfiguration).server_uuid[0]
-        PS > Add-ArubaCPServerCertificate -service_name RADIUS -server_uuid server_uuid -pkcs12_file_url http://192.0.2.1/PowerArubaCP.pfx -pkcs12_passphrase $passphrase
+        PS > Add-ArubaCPServerCertificate -service_name RADIUS -server_uuid $server_uuid -pkcs12_file_url http://192.0.2.1/PowerArubaCP.pfx -pkcs12_passphrase $passphrase
 
-        Add certificate (pfx) for service RADIUS on CPPM Server with uuid from Get-ArubaCPServerConfiguration using passphrase
+        Add certificate (pfx) for service RADIUS on CPPM Server with uuid (multiple server) from Get-ArubaCPServerConfiguration using passphrase
 
         .EXAMPLE
         $server_uuid = (Get-ArubaCPServerConfiguration).server_uuid
